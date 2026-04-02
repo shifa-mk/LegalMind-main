f6ecc9f8faabc8bf6ec3d11a9cb4c3f2dcfca641
shifa-mk <shifamk.xc@gmail.com>
2026-04-02 19:34:51 +0530

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:5001",
+  baseURL: process.env.REACT_APP_API_BASE_URL || "https://legalmind-backend-83kj.onrender.com",
   withCredentials: true,
 });
 
