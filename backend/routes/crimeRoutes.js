e3c5e32fd285299dd5c20e141db0393895c90959
shifa-mk <shifamk.xc@gmail.com>
2026-04-07 16:53:57 +0530

    Update crimeRoutes.js

--- backend/routes/crimeRoutes.js
+++ backend/routes/crimeRoutes.js
@@ -4,5 +4,8 @@ import { getCrimeData } from "../controllers/crimeController.js";
 const router = express.Router();
 
 router.post("/crime-data", getCrimeData);
+router.get("/test", (req, res) => {
+  res.send("Crime route working ✅");
+});
 
 export default router;
