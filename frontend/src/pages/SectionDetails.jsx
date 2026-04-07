f699ad84c66b1e9972891ec45dee53041764a957
shifa-mk <shifamk.xc@gmail.com>
2026-04-07 12:35:48 +0530

    Update SectionDetails.jsx

--- frontend/src/pages/SectionDetails.jsx
+++ frontend/src/pages/SectionDetails.jsx
@@ -165,18 +165,18 @@ return (
     <p>{section.relatedSections.join(", ")}</p>
   </div>
 )}
-  {sec.referenceLink && (
-            <div className="mb-3">
-              <a
-                href={sec.referenceLink}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-blue-600 underline"
-              >
-                📘 View Reference
-              </a>
-            </div>
-          )}
+{section.referenceLink && (
+  <div className="mb-3">
+    <a
+      href={section.referenceLink}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="text-blue-600 underline"
+    >
+      📘 View Reference
+    </a>
+  </div>
+)}
 {/* Important Cases */}
 {section.importantCases?.length > 0 && (
   <div className="mt-3">
