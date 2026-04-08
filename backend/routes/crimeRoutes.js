e5a93e25222d066cd3b1d177a0ad3fab9ea434ab
shifa-mk <shifamk.xc@gmail.com>
2026-04-08 13:33:53 +0530

    Update crimeRoutes.js

--- backend/routes/crimeRoutes.js
+++ backend/routes/crimeRoutes.js
@@ -2,8 +2,8 @@ import express from 'express';
 const router = express.Router();
 
 // 💡 Important: Use the .js extension in the import path
-import { getCrimeStats } from '../controllers/crime.controller.js';
-
+// Change 'controllers' to 'Controllers' to match your folder name
+import { getCrimeStats } from '../Controllers/crimeController.js';
 router.get('/stats-by-section', getCrimeStats);
 
 router.get("/test", (req, res) => {
